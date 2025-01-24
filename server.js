7846402fe9be7ad9c65f12765c8feec00f20f389
Jasdeep2k15 <singhjasdeep1602@gmail.com>
2025-01-24 14:26:54 +0530

    render setup

--- server.js
+++ server.js
@@ -17,7 +17,12 @@ app.use(express.json());
 // };
 
 // app.use(cors(corsOptions));
-app.use(cors());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 5000;
 
